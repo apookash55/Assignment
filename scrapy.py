1d73caf85854bfb2db82cbe1bef9c61ddc7303ec
apookash55 <44578360+apookash55@users.noreply.github.com>
2019-07-12 12:46:06 +0530

    Succesfully created the dataset
    Using scaper.py I have created the dataset using bs4

---
+++ scrapy.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import lxml
+sauce = urllib.request.urlopen('https://karki23.github.io/Weather-Data/assignment.html').read()
+srccode = BeautifulSoup(sauce,'lxml')
+l=srccode.find_all('a')
+links=[]
+city_names=[]
+for i in l:
+    links.append(i.get('href'))
+    city_names.append(i.string)
+for k in range(len(links)):
+        file = '.\\dataset\\' + city_names[k] + '.csv'
+        f=open(file,'w')
+        sauce = urllib.request.urlopen('https://karki23.github.io/Weather-Data/' + links[k]).read()
+        srccode = BeautifulSoup(sauce,'lxml')
+        table = srccode.table
+        for i in table.find_all('tr')[0:1]:
+            line = []
+            for j in i.find_all('th'):
+                line.append(j.string)
+            line = ','.join(line)
+            f.write(line + '\n')
+        for i in table.find_all('tr')[1:]:
+            line = []
+            for j in i.find_all('td'):
+                line.append(j.string)
+            line = ','.join(line)
+            f.write(line + '\n')
+        f.flush()
+        print(city_names[k] + '.csv','file written')
+        f.close()
